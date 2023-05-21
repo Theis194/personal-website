7869f82c3bfb03776848dbc5fcfa1ea17f960ed1
Theis194 <53541469+Theis194@users.noreply.github.com>
2023-05-21 14:31:54 +0200

    Started cookbook

---
+++ http/public/js/index.js
@@ -0,0 +1,16 @@
+function createRecipeListItems() {
+    let recipeList = document.querySelector("#recipe-list");
+    let recipeListItem = recipeList.querySelector(".recipelistitem")
+    let newRLI = recipeListItem.cloneNode(true);
+    console.log(newRLI );
+    newRLI.querySelector("#recipe1desc").textContent = "Cool shit!";
+    newRLI.addEventListener("click", changePage)
+
+    recipeList.appendChild(newRLI);
+}
+
+window.load = createRecipeListItems();
+
+function changePage() {
+    window.location.href = "../html/recipePage.html";
+}
