fdbda569d1a899d36daa99d0a52811eacc5fb8e2
Theis194 <53541469+Theis194@users.noreply.github.com>
2023-05-26 13:25:05 +0200

    login- and singupPage Done

---
+++ http/public/js/signupPage.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    // Intercept the form submission
+    $('#signupForm').submit(function(e) {
+      e.preventDefault(); // Prevent the default form submission
+  
+      // Serialize the form data
+      var formData = $(this).serialize();
+  
+      // Send an AJAX request
+      $.ajax({
+        type: 'POST',
+        url: $(this).attr('action'),
+        data: formData,
+        success: function(response, status, xhr) {
+            if (typeof response === "object") {
+                console.log(response); // This is where alerting the user with what was wrong needs to go
+                return false;
+            }
+            window.location.href = response;
+        },
+        error: function(xhr, status, error) {
+          // Handle errors
+          console.error(error);
+        }
+      });
+    });
+});
