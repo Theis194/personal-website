b532094a96421b419a8b98aef8a63a8417ffcfa2
Theis194 <53541469+Theis194@users.noreply.github.com>
2023-05-29 18:00:51 +0200

    debugging 2

--- http/public/js/loginPage.js
+++ http/public/js/loginPage.js
@@ -13,7 +13,8 @@ $(document).ready(function() {
         data: formData,
         success: function(response, status, xhr) {
             document.cookie = `${response.cookieName}=${JSON.stringify(response.cookieValue)}; max-age=${response.cookieOptions.maxAge}; secure=${response.cookieOptions.secure}; sameSite=${response.cookieOptions.sameSite}; path=${response.cookieOptions.path}`;
-            window.location.href = "/";
+            console.log(`${response.cookieName}=${JSON.stringify(response.cookieValue)}; max-age=${response.cookieOptions.maxAge}; secure=${response.cookieOptions.secure}; sameSite=${response.cookieOptions.sameSite}; path=${response.cookieOptions.path}`);
+            //window.location.href = "/";
         },
         error: function(xhr, status, error) {
           // Handle errors
