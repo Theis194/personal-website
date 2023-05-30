b1cf4af96e4d4009fe4234cec61befbfec2ba273
Theis194 <53541469+Theis194@users.noreply.github.com>
2023-05-30 20:54:43 +0200

    Redirects unpriviliged users

--- http/public/js/credentials.js
+++ http/public/js/credentials.js
@@ -9,6 +9,8 @@ function setNavBar() {
         activeUser = true
     }
 
+    checkCredentials();
+
     $(function(){
         $("#includedNav").load("/html/navBar/navbar.html", function() {
             authorRemovables.push("#nyOpskrift");
@@ -50,18 +52,17 @@ function setNavBar() {
 function checkCredentials() {
     let currentUser = JSON.parse(JSON.parse(getCookie("currentUser")));
 
-    if (currentUser.privileges.length > 0) {
-        if (!currentUser.privileges.includes("author")) {
-            for (let i = 0; i < authorRemovables.length; i++) {
-                authorRemovables[i].remove();
-            }
-        }
-        if (!currentUser.privileges.includes("admin")) {
-            for (let i = 0; i < authorRemovables.length; i++) {
-                authorRemovables[i].remove();
-            }
-        }
-    }  
+    // Add if statements that relocates the user to the index.html if they do not have the right privileges
+    // E.g. if they do not have admin privileg and try to access adminPage.html
+    let currentPage = window.location.href.split("/");
+    currentPage = currentPage[4];
+    if (currentPage != undefined) {
+        if (currentPage === "newRecipe.html" && !currentUser.privileges.includes("author")) {
+            document.location.href = "/";
+        } else if (currentPage === "adminPage.html" && !currentUser.privileges.includes("admin")) {
+            document.location.href = "/";
+        } // add favoritesPage once it is made but only check if currentUser != null
+    }
 }
 
 function getCookie(cookieName) {
